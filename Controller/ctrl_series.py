b96c187c5b6c69ac9e09d5796888b842a4cbaf7d
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2020-11-06 17:12:38 -0300

    Update ctrl_series.py
    Alterado para estar de acordo com o Filmes

--- Controller/ctrl_series.py
+++ Controller/ctrl_series.py
@@ -3,9 +3,25 @@ from View.serie_view import SerieView
 
 class CtrlSeries:
 
-    def __init__(self, CtrlPrincipal):
-        self.ctrlPrincipal = CtrlPrincipal
+    def __init__(self):
         self.serieView = SerieView(self)
+        self.series = Series
+        self.seriesList = []
 
     def telaSerie(self):
+        self.serieView.telaSerie()
+    
+    def inserirSerie(self, nome, nota, episodio,critica,genero):
+        series = Series(nome, nota, episodios, critica, genero)
+        self.__series.append(series)
+        
+    def excluirSerie(self, serieNome):
+        self.seriesList.remove(serieNome)
+        self.serieView.telaSerie()
+        
+    def ordenaSerie(self):
+        self.filmeView.telaFilme()
+        
+    def listarSerie(self):
+        print('[%s]' % ', '.join(map(str, self.seriesList)))
         self.serieView.telaSerie()
