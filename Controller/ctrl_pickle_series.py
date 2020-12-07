7ad213faadcd8b1346ab3109ce4ae82f188b8bb3
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2020-12-07 05:28:43 -0300

    Estrutura inicial do pickle

---
+++ Controller/ctrl_pickle_series.py
@@ -0,0 +1,10 @@
+import pickle
+
+series = {}
+arq_series = open('series.pkl', 'wb')
+
+def metodo_dump():
+  pickle.dump(series, arq_series)
+  
+def metodo_load():
+  pickle.load(series,arq_series)
